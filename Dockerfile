b63d6c6764e3b323db6d8d6d78de68d4b9683221
skantay <skantay03@gmail.com>
2024-03-23 16:14:54 +0600

    upladoing

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM golang:alpine as modules
+COPY go.mod go.sum /modules/
+WORKDIR /modules
+RUN go mod download
+
+
+FROM golang:alpine as builder
+COPY --from=modules /go/pkg /go/pkg
+COPY . /app
+WORKDIR /app
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 \
+    go build -tags migrate -o /bin/app ./cmd/app
+
+FROM scratch
+COPY --from=builder /app/config /config
+COPY --from=builder /app/migrations /migrations
+COPY --from=builder /bin/app /app
+COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
+CMD ["/app"]
---
+++ pkg/httpserver/server.go
@@ -0,0 +1,63 @@
+package httpserver
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+const (
+	defaultReadTimeout     = 10 * time.Second
+	defaultWriteTimeout    = 10 * time.Second
+	defaultAddr            = ":80"
+	defaultIdleTimeout     = 10 * time.Second
+	defaultShutdownTimeout = 3 * time.Second
+)
+
+type Server struct {
+	server          *http.Server
+	notify          chan error
+	shutdownTimeout time.Duration
+}
+
+func New(handler http.Handler, opts ...Option) *Server {
+	httpServer := &http.Server{
+		Handler:      handler,
+		ReadTimeout:  defaultReadTimeout,
+		WriteTimeout: defaultWriteTimeout,
+		IdleTimeout:  defaultIdleTimeout,
+		Addr:         defaultAddr,
+	}
+
+	s := &Server{
+		server:          httpServer,
+		notify:          make(chan error, 1),
+		shutdownTimeout: defaultShutdownTimeout,
+	}
+
+	for _, opt := range opts {
+		opt(s)
+	}
+
+	s.start()
+
+	return s
+}
+
+func (s *Server) start() {
+	go func() {
+		s.notify <- s.server.ListenAndServe()
+		close(s.notify)
+	}()
+}
+
+func (s *Server) Notify() <-chan error {
+	return s.notify
+}
+
+func (s *Server) Shutdown() error {
+	ctx, cancel := context.WithTimeout(context.Background(), s.shutdownTimeout)
+	defer cancel()
+
+	return s.server.Shutdown(ctx)
+}
